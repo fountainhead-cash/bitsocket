5d9917af6e1634ba34600c67734345ed3d7a4f7f
blockparty <hello@blockparty.sh>
2019-01-05 04:31:46 -0600

    load config from .env

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
 const bitsocketd = require('fountainhead-bitsocketd')
 bitsocketd.init({
-    bit: { host: "127.0.0.1", port: 28339 },
-    socket: { port: 3001 }
+    bit: {
+        host: process.env.zmq_outgoing_host ? process.env.zmq_outgoing_host : '0.0.0.0',
+        port: Number.parseInt(process.env.zmq_outgoing_port ? process.env.zmq_outgoing_port : 28339)
+    },
+    socket: {
+        port: process.env.bitsocket_port ? process.env.bitsocket_port : 3000
+    }
 });
 
